31e82eb0904930949240c8e084a2e85088e44fcd
Armando Peña Tamayo (Ender) <ender@aleph.engineering>
2023-01-10 22:05:59 -0500

    Refactoring tests

--- src/__tests__/formatter.test.ts
+++ src/__tests__/formatter.test.ts
@@ -1,18 +1,18 @@
-import { format, title, subtitle, error, warning, info } from '../';
+import { custom, title, sub, err, warn, info } from '../';
 
 test('Formatter on correct parameters', () => {
-  const text = `${format('Hello', { color: 'green', bg: 'yellow', style: 'bold' })} World`;
-  expect(text).toEqual('🚀 \x1b[32m\x1b[43m\x1b[1mHello\x1b[0m World');
+  const text = `${custom('Hello', 'green', 'bgYellow', 'bold')} World`;
+  expect(text).toEqual('\u001b[1m\u001b[43m\u001b[32mHello\u001b[0m\u001b[0m\u001b[0m World');
 });
 
 test('Formatters to work without text', () => {
-  const text1 = `${format('', { color: 'green', bg: 'yellow', style: 'bold' })} World`;
+  const text1 = `${custom('', 'green', 'bgYellow', 'bold')} World`;
   const text2 = `${title('')} World`;
-  const text3 = `${subtitle('')} World`;
-  const text4 = `${error('')} World`;
-  const text5 = `${warning('')} World`;
+  const text3 = `${sub('')} World`;
+  const text4 = `${err('')} World`;
+  const text5 = `${warn('')} World`;
   const text6 = `${info('')} World`;
-  const result = '🚀 World';
+  const result = ' World';
   expect(text1).toEqual(result);
   expect(text2).toEqual(result);
   expect(text3).toEqual(result);
@@ -22,31 +22,37 @@ test('Formatters to work without text', () => {
 });
 
 test('Formatter with no formats', () => {
-  const text = `${format('Hello')} World`;
-  expect(text).toEqual('🚀 Hello World');
+  const text = `${custom('Hello')} World`;
+  expect(text).toEqual('Hello World');
 });
 
 test('Title with text', () => {
   const text = `${title('Hello')} World`;
-  expect(text).toEqual(`🚀 \x1b[42m\x1b[97m\x1b[1m HELLO \x1b[0m World`);
+  expect(text).toEqual(`🚀 \u001b[1m\u001b[97m\u001b[42m Hello \u001b[0m\u001b[0m\u001b[0m World`);
 });
 
 test('Subtitle with text', () => {
-  const text = `${subtitle('Hello')} World`;
-  expect(text).toEqual(`#️⃣ \x1b[36m\x1b[3m HELLO \x1b[0m World`);
+  const text = `${sub('Hello')} World`;
+  expect(text).toEqual(`#️⃣  \u001b[4m\u001b[36mHello\u001b[0m\u001b[0m World`);
 });
 
 test('Error with text', () => {
-  const text = `${error('Hello')} World`;
-  expect(text).toEqual(`🔴 \x1b[41m\x1b[97m\x1b[1m ERROR: \x1b[0m \x1b[31m\x1b[4mHello\x1b[0m World`);
+  const text = `${err('Hello')} World`;
+  expect(text).toEqual(
+    `🔴 \u001b[1m\u001b[97m\u001b[41m ERROR: \u001b[0m\u001b[0m\u001b[0m \u001b[31mHello\u001b[0m World`,
+  );
 });
 
 test('Warning with text', () => {
-  const text = `${warning('Hello')} World`;
-  expect(text).toEqual(`🌡️  \x1b[43m\x1b[97m\x1b[1m WARNING: \x1b[0m \x1b[33mHello\x1b[0m World`);
+  const text = `${warn('Hello')} World`;
+  expect(text).toEqual(
+    `🌡️  \u001b[1m\u001b[97m\u001b[43m WARNING: \u001b[0m\u001b[0m\u001b[0m \u001b[33mHello\u001b[0m World`,
+  );
 });
 
 test('Info with text', () => {
   const text = `${info('Hello')} World`;
-  expect(text).toEqual(`👀 \x1b[46m\x1b[97m\x1b[1m INFO: \x1b[0m \x1b[36mHello\x1b[0m World`);
+  expect(text).toEqual(
+    `👀 \u001b[1m\u001b[97m\u001b[46m INFO: \u001b[0m\u001b[0m\u001b[0m \u001b[36mHello\u001b[0m World`,
+  );
 });
